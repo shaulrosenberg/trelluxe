e8aa1d8aabc68fd2319bcc156c14e0f588479f7b
Dor cohen <78268224+plutus777@users.noreply.github.com>
2023-06-01 11:58:15 +0300

    clean some css files, also added home-page-layout in layouts css file

--- frontend/src/pages/home-page.jsx
+++ frontend/src/pages/home-page.jsx
@@ -1,13 +1,27 @@
 import React from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import logo from '../assets/img/logo.png'
-import { CHANGE_COUNT } from '../store/user.reducer'
+// import { useDispatch, useSelector } from 'react-redux'
+// import logo from '../assets/img/logo.png'
+// import { CHANGE_COUNT } from '../store/user.reducer'
+import heroImg from '../assets/img/hero-trello.png';
 
 export function HomePage() {
-
-    return (
-        <section>
-            <h1>Hello</h1>
-        </section >
-    )
+     return (
+          <section className='home-page-layout'>
+               <div className='div-hero-section'>
+                    <div>
+                         <h1>
+                              Trello brings all your tasks, teammates, and tools
+                              together
+                         </h1>
+                         <h2>
+                              Keep everything in the same place—even if your
+                              team isn’t.
+                         </h2>
+                    </div>
+                    <div>
+                        <img src={heroImg}/>
+                    </div>
+               </div>
+          </section>
+     )
 }
