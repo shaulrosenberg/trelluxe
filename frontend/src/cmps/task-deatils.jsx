a2b3aea75a9cc907075a48dbad864917942b7bce
Dor cohen <78268224+plutus777@users.noreply.github.com>
2023-06-02 12:04:43 +0300

    update the ui in task details, files: task-deatils.jsx & _task.details.scss

--- frontend/src/cmps/task-deatils.jsx
+++ frontend/src/cmps/task-deatils.jsx
@@ -1,11 +1,19 @@
 import { useEffect } from 'react'
 import { useParams } from 'react-router'
+import { useNavigate } from 'react-router-dom'
 import { boardService } from '../services/board.service'
 import { useState } from 'react'
+
+// icons
+import { IoIosCard } from 'react-icons/io'
+import { TfiAlignLeft } from 'react-icons/tfi'
+import { TfiMenuAlt } from 'react-icons/tfi'
+import { AiOutlineClose } from 'react-icons/ai'
 
 export function TaskDetails() {
      const { taskId } = useParams()
      const [task, setTask] = useState(null)
+     const navigate = useNavigate();
 
      useEffect(() => {
           boardService
@@ -13,14 +21,40 @@ export function TaskDetails() {
                .then((currTask) => setTask(currTask))
      }, [taskId])
 
+     function onTaskExit() {
+        navigate(-1);
+     }
+
      return (
           <section className='section-task-deatils'>
                <div className='div-task-deatils'>
-                    {task ? <h2>{task.title}</h2> : 'Loading'}
-                    <p>Description</p>
-                    <a>Add a more detailed description...</a>
-                    <p>Activity</p>
-                    <input placeholder='Write a comment...'></input>
+                    {/* <button className='btn-exit-task'>X</button> */}
+                    <AiOutlineClose
+                         className='btn-exit-task'
+                         onClick={() => onTaskExit()}
+                    />
+                    <IoIosCard className='icon-title' />
+                    <div className='div-task-title'>
+                         {task ? <h2>{task.title}</h2> : 'Loading'}
+                         <p>in list group 1</p>
+                    </div>
+                    <TfiAlignLeft className='icon-desc' />
+                    <div className='div-desc'>
+                         <p>Description</p>
+                         <a className='a-desc'>
+                              Add a more detailed description...
+                         </a>
+                    </div>
+                    <TfiMenuAlt className='icon-activitiy' />
+                    <div className='a'></div>
+                    <h5 className='task-details-user'>User</h5>
+                    <div className='div-activity'>
+                         <p>Activity</p>
+                         <input
+                              className='input-task-activity'
+                              placeholder='Write a comment...'
+                         ></input>
+                    </div>
                </div>
           </section>
      )
