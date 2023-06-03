0a5d7aa26b8aa937baa90b26642d377400b04daf
Dor cohen <78268224+plutus777@users.noreply.github.com>
2023-06-03 20:59:56 +0300

    added functionality in attach-content.jsx

--- frontend/src/cmps/dynamic-modal/attach-content.jsx
+++ frontend/src/cmps/dynamic-modal/attach-content.jsx
@@ -1,14 +1,55 @@
+import { uploadService } from '../../services/upload.service'
+import { boardService } from '../../services/board.service'
+import { updateTask } from '../../store/board.actions'
 
-
-
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
 
 export function AttachContent() {
+     const params = useParams()
+     const [taskId, setTaskId] = useState(null)
+     const [boardId, setBoardId] = useState(null)
+     const [currTask, setCurrTask] = useState(null)
 
-    return(
-        <section>
-            <p>Attach Pic</p>
-            <p>Attach File</p>
-            <p>Attach Whatever</p>
-        </section>
-    )
+     // get the curr task by the url task id
+     // get the board id
+     // get the group id
+
+     async function saveImg(ev) {
+          try {
+               const imgUrl = await uploadService.uploadImg(ev)
+               console.log(imgUrl)
+               console.log('currTask', currTask)
+               currTask.imgAttachment = imgUrl
+
+               await updateTask(currTask, params.boardId, 'g104')
+          } catch (err) {
+               console.log('err saving img', err)
+          }
+     }
+
+     useEffect(() => {
+         //   setTaskId(params.taskId)
+         //   setBoardId(params.boardId)
+          console.log('cool params', params)
+          boardService.findTaskById(params.taskId).then((task) => {
+            setCurrTask(task)
+          })
+     }, [])
+
+     //  async function s() {
+     //       console.log(taskId)
+     //       const cTask = await boardService.findTaskById(taskId)
+     //       console.log('cTask', cTask)
+     //       setCurrTask(cTask)
+     //  }
+
+     return (
+          <section>
+               <p>Attach File</p>
+               <input type='file' onChange={saveImg} />
+               <p>Attach Pic</p>
+               <p>Attach Whatever</p>
+          </section>
+     )
 }
