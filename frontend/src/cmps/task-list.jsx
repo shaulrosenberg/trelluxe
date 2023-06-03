e89a557e934eafe55f4698da6412cf10c7d7d8b3
shaulrosenberg <shaulr10@gmail.com>
2023-06-03 21:19:41 +0300

    updated task-list

--- frontend/src/cmps/task-list.jsx
+++ frontend/src/cmps/task-list.jsx
@@ -4,15 +4,15 @@ import { useNavigate } from "react-router-dom"
 export function TaskList({ tasks, groupId, boardId }) {
     const navigate = useNavigate()
 
-    
+
 
     console.log('tasks:', tasks)
     return (
         <section className="task-list-container">
             {tasks.map((task) => {
                 return (
-                    <section onClick={() => navigate(`group/${groupId}/task/${task.id}`)}>
-                        <TaskPreview key={task.id} task={task} />
+                    <section key={task.id} onClick={() => navigate(`group/${groupId}/task/${task.id}`)}>
+                        <TaskPreview task={task} />
                     </section>
                 )
             })}
