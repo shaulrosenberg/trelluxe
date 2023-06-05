544c1b9c518d3eeada667c5c0c1ebe491d104f14
gertzMan <101058145+gertzMan@users.noreply.github.com>
2023-06-05 11:41:27 +0300

    task preview basic layout

--- frontend/src/cmps/task-preview.jsx
+++ frontend/src/cmps/task-preview.jsx
@@ -2,7 +2,17 @@ export function TaskPreview(props) {
     const { task } = props
     return (
         <div className="task-preview-container">
-            <div className="title-container"><p>{task.title}</p></div>
+            {/* {task.style.backgroundColor &&
+                <section className='task-preview-cover'></section>} */}
+            <section className='task-preview-body'>
+                <div className="task-preview-labels"></div>
+                <div className="title-container"><p>{task.title}</p></div>
+                <div className="task-preview-badges"></div>
+            </section>
+            <div className="quick-edit"></div>
+
+
+
         </div>
     )
 }
