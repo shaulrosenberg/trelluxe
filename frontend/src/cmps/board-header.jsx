5cb74c31194e19aae34b3f2a3d5fab517e2516da
Dor cohen <78268224+plutus777@users.noreply.github.com>
2023-06-05 21:18:01 +0300

    made the miniheader bg color transparent

--- frontend/src/cmps/board-header.jsx
+++ frontend/src/cmps/board-header.jsx
@@ -1,92 +1,100 @@
-import { IoStarOutline, IoPersonAddOutline, IoEllipsisHorizontalSharp } from 'react-icons/io5';
-import { BsFilter } from "react-icons/bs"
-import { AiFillStar, AiOutlineStar } from 'react-icons/ai';
+import {
+     IoStarOutline,
+     IoPersonAddOutline,
+     IoEllipsisHorizontalSharp,
+} from 'react-icons/io5'
+import { BsFilter } from 'react-icons/bs'
+import { AiFillStar, AiOutlineStar } from 'react-icons/ai'
 import { useEffect, useState } from 'react'
-import { darken } from 'polished'
+import { darken, transparentize} from 'polished'
 import { boardService } from '../services/board.service'
 import { Link, NavLink, useLocation, Route, useParams } from 'react-router-dom'
-import PropTypes from 'prop-types';
-
+import PropTypes from 'prop-types'
 
 export function BoardHeader({ board }) {
-    const location = useLocation()
+     const params = useParams()
+     const [navColor, setNavColor] = useState(null)
 
-    const [navColor, setNavColor] = useState(null)
-    const boardId = location.pathname.split('/board/')[1]
-    useEffect(() => {
-        fetchBoardStyle()
-    }, [location])
+     useEffect(() => {
+          fetchBoardStyle()
+     })
 
-    function fetchBoardStyle() {
-        if (boardId) {
-            boardService
-                .getById(boardId)
-                .then((board) => {
+     function fetchBoardStyle() {
+          boardService
+               .getById(params.boardId)
+               .then((board) => {
                     let boardStyleColor = board.style.backgroundColor
                     boardStyleColor = darken(0.1, boardStyleColor)
+                    // const transparentColor = `rgba(${boardStyleColor}, 0.2)`
+                    const transparentColor = transparentize(0.7, boardStyleColor);
 
-                    setNavColor(boardStyleColor)
-                })
-                .catch((err) =>
-                    console.log('failed to change nav color', err)
-                )
-        } else {
-            setNavColor('#026AA7')
-        }
-    }
+                    setNavColor(transparentColor)
+               })
+               .catch((err) => console.log('failed to change nav color', err))
+     }
 
-    return (
-        <header className="board-header" style={{ backgroundColor: navColor }}>
+     return (
+          <header
+               className='board-header'
+               style={{ backgroundColor: navColor }}
+          >
+               <div className='left'>
+                    <h1 className='board-title'>{board?.title}</h1>
 
+                    {board.isStarred ? (
+                         <IconButton
+                              Icon={AiFillStar}
+                              text=''
+                              onClick={() => console.log('Clicked!')}
+                         />
+                    ) : (
+                         <IconButton
+                              Icon={AiOutlineStar}
+                              text=''
+                              onClick={() => console.log('Clicked!')}
+                         />
+                    )}
+               </div>
 
-            <div className="left">
-                <h1 className="board-title">{board?.title}</h1>
-
-                {(board.isStarred) ?
-                    <IconButton Icon={AiFillStar} text="" onClick={() => console.log('Clicked!')} />
-                    :
-                    <IconButton Icon={AiOutlineStar} text="" onClick={() => console.log('Clicked!')} />
-
-                }
-
-
-
-            </div>
-
-
-            <div className="right">
-
-
-                <IconButton Icon={BsFilter} text="Filter" onClick={() => console.log('Clicked!')} />
-                <IconButton Icon={IoPersonAddOutline} text="Share" className='share-board-btn ' onClick={() => console.log('Clicked!')} />
-                <IconButton Icon={IoEllipsisHorizontalSharp} text="" onClick={() => console.log('Clicked!')} />
-
-            </div>
-
-
-
-        </header >
-    )
-
-
+               <div className='right'>
+                    <IconButton
+                         Icon={BsFilter}
+                         text='Filter'
+                         onClick={() => console.log('Clicked!')}
+                    />
+                    <IconButton
+                         Icon={IoPersonAddOutline}
+                         text='Share'
+                         className='share-board-btn '
+                         onClick={() => console.log('Clicked!')}
+                    />
+                    <IconButton
+                         Icon={IoEllipsisHorizontalSharp}
+                         text=''
+                         onClick={() => console.log('Clicked!')}
+                    />
+               </div>
+          </header>
+     )
 }
 
-
-const IconButton = ({ Icon, text = '', style = {}, className = 'svg-btn', ...props }) => {
-    const defaultStyle = {
-
-    };
-    IconButton.propTypes = {
-        Icon: PropTypes.elementType.isRequired,
-        text: PropTypes.string,
-        style: PropTypes.object,
-    };
-    return (
-        <button style={defaultStyle} className={className} {...props}>
-            <Icon style={{ marginRight: text ? '8px' : '0' }} />
-            {text}
-        </button>
-    );
-
-
-};
+const IconButton = ({
+     Icon,
+     text = '',
+     style = {},
+     className = 'svg-btn',
+     ...props
+}) => {
+     const defaultStyle = {}
+     IconButton.propTypes = {
+          Icon: PropTypes.elementType.isRequired,
+          text: PropTypes.string,
+          style: PropTypes.object,
+     }
+     return (
+          <button style={defaultStyle} className={className} {...props}>
+               <Icon style={{ marginRight: text ? '8px' : '0' }} />
+               {text}
+          </button>
+     )
+}
