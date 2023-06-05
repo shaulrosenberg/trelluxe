4c1404bf44f123710d1e01fd03501ad40c3dc2fa
shaulrosenberg <shaulr10@gmail.com>
2023-06-05 14:52:17 +0300

    task-preview cover added

--- frontend/src/cmps/task-preview.jsx
+++ frontend/src/cmps/task-preview.jsx
@@ -1,14 +1,25 @@
 export function TaskPreview(props) {
     const { task } = props
 
-    
+    const hasBackgroundImage = task.style?.backgroundImage
+    const hasBackgroundColor = task.style?.backgroundColor
 
     return (
         <div className="task-preview-container">
-            {/* {task.style.backgroundColor &&
-                <section className='task-preview-cover'></section>} */}
+             {task.style &&
+                <section
+                    className='task-preview-cover'
+                    style={{ backgroundColor: hasBackgroundImage ? null : hasBackgroundColor }}
+                >
+                    {hasBackgroundImage && <img src={task.style.backgroundImage} alt="Task Cover" />}
+                </section>
+            }
+            {!task.style && task.imgAttachment && <section
+                    className='task-preview-cover'><img src={task.imgAttachment} alt="Task Cover" /></section>}
             <section className='task-preview-body'>
-                <div className="task-preview-labels"></div>
+                <div className="task-preview-labels">
+                    
+                </div>
                 <div className="title-container"><p>{task.title}</p></div>
                 <div className="task-preview-badges"></div>
             </section>
