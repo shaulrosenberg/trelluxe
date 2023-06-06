533e90153026c20c0f78127411f4d4526bf1c1ae
Dor cohen <78268224+plutus777@users.noreply.github.com>
2023-06-06 12:02:16 +0300

    added some styling to group preview

--- frontend/src/cmps/task-attachment.jsx
+++ frontend/src/cmps/task-attachment.jsx
@@ -1,20 +1,23 @@
 import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { boardService } from '../services/board.service'
+
 // icon
 import { ImAttachment } from 'react-icons/im'
+import { MdSubtitles } from 'react-icons/md'
 
 export function AttachImage({ task }) {
      return (
           <div className='img-attachment-container'>
-               {task.imgAttachment !== '' && <p>Attachment</p>}
-               {task.imgAttachment !== '' ? (
+               {task.imgAttachment && <p>Attachment</p>}
+               {task.imgAttachment ? (
                     <div className='attachment-container'>
                          <img
                               src={task.imgAttachment}
                               alt='Attachment'
                               className='img-attachment'
                          />
+                         <h3><span> <MdSubtitles className='cover-icon'/> </span> Remove cover</h3>
                     </div>
                ) : null}
           </div>
