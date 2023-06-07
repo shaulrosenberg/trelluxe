d85b4d02826549e5065c9b58fa65aada8085b63c
shaulrosenberg <shaulr10@gmail.com>
2023-06-07 22:38:42 +0300

    task-details refactor

--- frontend/src/cmps/task-overview.jsx
+++ frontend/src/cmps/task-overview.jsx
@@ -1,13 +1,25 @@
 import { FiPlus } from 'react-icons/fi'
 import { boardService } from '../services/board.service'
 import { useSelector } from 'react-redux'
-import { useEffect } from 'react'
+import { DynamicActionModal } from './dynamic-modal/dynamic-action-modal'
+import { useEffect, useRef, useState } from 'react'
 export function TaskOverview({ task, groupId, boardId }) {
+
+    const [modalType, setModalType] = useState(null)
+    const eventRef = useRef()
+
     const board = useSelector(storeState => storeState.boardModule.selectedBoard)
 
     const taskMembers = boardService.getTaskMembers(board.members, task.memberIds)
 
+    function onToggleModal (ev){
+        eventRef.current = ev
+        setModalType('members')
+    }
 
+    function onCloseModal() {
+        setModalType(null)
+    }
 
     return (
 
@@ -30,9 +42,17 @@ export function TaskOverview({ task, groupId, boardId }) {
                         </li>)}
 
                         <li key="" className='' >
-                            <FiPlus />
+                            <FiPlus onClick={onToggleModal}/>
                         </li>
                     </ul>
+                    {modalType && <DynamicActionModal 
+                    cmpType={modalType} 
+                    event={eventRef.current} 
+                    task={task} 
+                    groupId={groupId} 
+                    boardId={boardId}
+                    onCloseModal={onCloseModal}
+                    />}
                 </>
 
 
