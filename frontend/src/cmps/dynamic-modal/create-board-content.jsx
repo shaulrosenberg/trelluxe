a6e05effc99efbf4933a4fa845e1ca3a683efa3b
Dor cohen <78268224+plutus777@users.noreply.github.com>
2023-06-10 17:54:48 +0300

    ui improvment on the workspace create board section

--- frontend/src/cmps/dynamic-modal/create-board-content.jsx
+++ frontend/src/cmps/dynamic-modal/create-board-content.jsx
@@ -1,64 +1,87 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { addBoard } from '../../store/board.actions'
 import boardPreview from '../../assets/img/board-preview.svg'
 
 export function CreateBoardContent({ onCloseModal }) {
-    const [boardTitle, setBoardTitle] = useState('')
-    const [boardBackground, setBoardBackground] = useState('#0079bf')  // default Trello board color
-    const navigate = useNavigate()
+   const [boardTitle, setBoardTitle] = useState('')
+   const [boardBackground, setBoardBackground] = useState('#0079bf') // default Trello board color
+   const navigate = useNavigate()
 
-    const backgrounds = [
-        '#0079bf', 
-        '#d29034', 
-        // 'linear-gradient(to right, #519839, #b04632)', 
-        '#89609e', 
-        // 'linear-gradient(to right, #cd5a91, #4bbf6b)', 
-        '#00aecc', 
-        '#838c91',
-        // 'linear-gradient(to right, #0079bf, #d29034)',
-        // 'linear-gradient(to right, #89609e, #838c91)'
-    ]
+   const backgrounds = ['#0079bf', '#d29034', '#89609e', '#00aecc', '#838c91', '#F1813A', '#B86BBF', '#b2d8d8'];
 
-    async function handleSubmit(event) {
-        event.preventDefault()
-        const newBoard = await addBoard({
-            title: boardTitle,
-            style: { backgroundColor: boardBackground },
-            isStarred: false,
-            groups: [],
-            activities: []
-        })
-        onCloseModal()
-        navigate(`/board/${newBoard._id}`)
-    }
+   async function handleSubmit(event) {
+      event.preventDefault()
+      const newBoard = await addBoard({
+         title: boardTitle,
+         style: { backgroundColor: boardBackground },
+         isStarred: false,
+         groups: [],
+         activities: [],
+      })
+      onCloseModal()
+      navigate(`/board/${newBoard._id}`)
+   }
 
-    return (
-        <section className="create-board-content">
-            {/* Board Preview */}
-            <div className="board-preview" style={{ backgroundColor: boardBackground }}>
-                <h3>{boardTitle || 'Untitled Board'}</h3>
+   useEffect(() => {
+      
+   }, [boardTitle])
+   const isAllow = !boardTitle ? 'not-allowed' : ''
+   return (
+      <section className='create-board-content'>
+         {/* Board Preview */}
+         <div
+            className='board-preview'
+            style={{ backgroundColor: boardBackground }}
+         >
+            {/* <h3>{boardTitle || 'Untitled Board'}</h3> */}
+            <img src='https://a.trellocdn.com/prgb/assets/14cda5dc635d1f13bc48.svg' />
+         </div>
+
+         {/* Board Creation Form */}
+         <form onSubmit={handleSubmit}>
+            <label htmlFor='boardBackground' className='workspace-text-style'>
+               Background
+            </label>
+            <div id='boardBackground' className='color-selector'>
+               {backgrounds.map((background) => (
+                  <div
+                     key={background}
+                     className='color-option'
+                     style={{ backgroundColor: background }}
+                     onClick={() => setBoardBackground(background)}
+                  ></div>
+               ))}
             </div>
 
-            {/* Board Creation Form */}
-            <form onSubmit={handleSubmit}>
-                <label htmlFor="boardTitle">Title</label>
-                <input id="boardTitle" type="text" value={boardTitle} onChange={(e) => setBoardTitle(e.target.value)} required />
+            <label className='workspace-text-style' htmlFor='boardTitle'>
+               Title
+            </label>
+            <input
+               id='boardTitle'
+               className='workspace-board-input'
+               type='text'
+               value={boardTitle}
+               onChange={(e) => setBoardTitle(e.target.value)}
+               required
+            />
+            {!boardTitle && (
+               <p
+                  className='workspace-text-style'
+                  style={{ fontWeight: 400, fontSize: '14px' }}
+               >
+                  👋 Board title is required
+               </p>
+            )}
 
-                <label htmlFor="boardBackground">Background</label>
-                <div id="boardBackground" className="color-selector">
-                    {backgrounds.map(background => (
-                        <div
-                            key={background}
-                            className="color-option"
-                            style={{ backgroundColor: background }}
-                            onClick={() => setBoardBackground(background)}
-                        ></div>
-                    ))}
-                </div>
-
-                <button type="submit">Create Board</button>
-            </form>
-        </section>
-    )
+            <button
+               style={{ cursor: isAllow }}
+               className='workspace-create-btn'
+               type='submit'
+            >
+               Create Board
+            </button>
+         </form>
+      </section>
+   )
 }
