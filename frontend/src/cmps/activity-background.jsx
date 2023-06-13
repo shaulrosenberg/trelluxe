8ea6979e7688584a1519ebfb6b471d262daa4475
Dor cohen <78268224+plutus777@users.noreply.github.com>
2023-06-13 16:33:25 +0300

    activity-content images improvment

--- frontend/src/cmps/activity-background.jsx
+++ frontend/src/cmps/activity-background.jsx
@@ -2,15 +2,18 @@ import { useState } from 'react'
 
 // cmp
 import { ActivityColors } from './activity-colors'
+import { ActivityImages } from './activity-images'
 
 export function ActivityBackgroundColor() {
    const [isColorsOpen, setIsColorsOpen] = useState(false)
-   const [isImagesOpen, setIsImageOpen] = useState(false)
+   const [isImagesOpen, setIsImagesOpen] = useState(false)
 
    return isColorsOpen ? (
       <ActivityColors />
+   ) : isImagesOpen ? (
+      <ActivityImages /> // Render the different component here
    ) : (
-      <section className='activity-background-container curser-pointer'>
+      <section className='activity-background-container cursor-pointer'>
          <div className='activity-style-container'>
             <div
                onClick={() => setIsColorsOpen(true)}
@@ -22,7 +25,7 @@ export function ActivityBackgroundColor() {
             {/* <h2 className='activity-text-style'>Colors</h2> */}
 
             <div
-               onClick={() => setIsImageOpen(true)}
+               onClick={() => setIsImagesOpen(true)}
                className='activity-background-images cursor-pointer'
             >
                <img src='https://cdn.pixabay.com/photo/2023/05/27/12/20/eastern-spinebill-8021395_1280.jpg' />
