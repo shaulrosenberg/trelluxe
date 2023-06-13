ff2f560da905353f907bf657e4986b99d43b34c0
Dor cohen <78268224+plutus777@users.noreply.github.com>
2023-06-13 19:43:47 +0300

    added functionality in the activity section to be able to change background image

--- frontend/src/cmps/activity-images.jsx
+++ frontend/src/cmps/activity-images.jsx
@@ -1,27 +1,52 @@
-import { useEffect, useState } from "react";
-import { unsplashService } from "../services/unsplash.service";
+import { useEffect, useState } from 'react'
+import { unsplashService } from '../services/unsplash.service'
+import { useSelector } from 'react-redux'
+import { updateBoard } from '../store/board.actions'
 
 export function ActivityImages() {
-    const [imagesObj, setImagesObj] = useState({});
+   const [imagesObj, setImagesObj] = useState([])
+   const board = useSelector(
+      (storeState) => storeState.boardModule.selectedBoard
+   )
 
-    useEffect(() => {
-        const fetchData = async () => {
-            const images = await unsplashService.searchPhotos('fantasy', 1, 6);
-            setImagesObj(images);
-        };
+   useEffect(() => {
+      const fetchData = async () => {
+         const images = await unsplashService.searchPhotos('fantasy', 1, 8)
+         setImagesObj(images)
+      }
 
-        fetchData();
-    }, []);
+      fetchData()
+   }, [])
 
-    console.log('images', imagesObj);
+   function onImageClick(imgUrl) {
+    console.log(imgUrl);
+    const { style, ...rest } = board;
+    const updatedBoard = {
+      ...rest,
+      style: {
+        ...style,
+        backgroundImage: imgUrl,
+      },
+    };
+    updateBoard(updatedBoard);
+  }
+  
 
-    return (
-        <section>
-            {imagesObj.map((image) => {
-                return <div>
-                    <img src={image.urls.small}/>
-                </div>
-            })}
-        </section>
-    );
+   console.log('images', imagesObj)
+   if (!imagesObj) return <div>loading</div>
+   return (
+      <section className='main-activity-images-section'>
+         {imagesObj.map((image) => {
+            return (
+               <div
+                  className='activity-images-container cursor-pointer'
+                  key={image.id}
+                  onClick={() => onImageClick(image.urls.full)}
+               >
+                  <img src={image.urls.small} />
+               </div>
+            )
+         })}
+      </section>
+   )
 }
