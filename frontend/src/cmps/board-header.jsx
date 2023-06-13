dc8d7eaa29ba1f67b3705b11ad60a9a5814e5440
Dor cohen <78268224+plutus777@users.noreply.github.com>
2023-06-13 14:19:26 +0300

    added members icons in board-header

--- frontend/src/cmps/board-header.jsx
+++ frontend/src/cmps/board-header.jsx
@@ -8,142 +8,147 @@ import { DynamicActionModal } from './dynamic-modal/dynamic-action-modal'
 
 //icons
 import { BsFilter } from 'react-icons/bs'
-import {IoStarOutline, IoPersonAddOutline, IoEllipsisHorizontalSharp,} from 'react-icons/io5'
+import {
+   IoStarOutline,
+   IoPersonAddOutline,
+   IoEllipsisHorizontalSharp,
+} from 'react-icons/io5'
 import { AiFillStar, AiOutlineStar } from 'react-icons/ai'
 import PropTypes, { func } from 'prop-types'
 
-export function BoardHeader({ board}) {
-     const params = useParams()
-     const [currTask, setCurrTask] = useState(null)
-     const [navColor, setNavColor] = useState(null)
-     
-     const [currBoard, setCurrboard] = useState(null)
-     const [modalType, setModalType] = useState(null)
+export function BoardHeader({ board }) {
+   const params = useParams()
+   const [currTask, setCurrTask] = useState(null)
+   const [navColor, setNavColor] = useState(null)
 
-     const eventRef = useRef()
+   const [currBoard, setCurrboard] = useState(null)
+   const [modalType, setModalType] = useState(null)
 
-     const modalTitles = {
-          'filter': 'Filter',
-          'activity': 'Menu',
-          'addMember': 'Add Member'
-     }
+   const eventRef = useRef()
 
-     function onToggleModal(type = null, ev = null) {
-          eventRef.current = ev
-          setModalType(type)
+   const modalTitles = {
+      filter: 'Filter',
+      activity: 'Menu',
+      addMember: 'Add Member',
+   }
+
+   function onToggleModal(type = null, ev = null) {
+      eventRef.current = ev
+      setModalType(type)
+   }
+
+   function onCloseModal() {
+      setModalType(null)
+   }
+
+   useEffect(() => {
+      fetchBoardStyle()
+      console.log(params)
+   }, [])
+
+   async function fetchBoardStyle() {
+      if (params.boardId) {
+         try {
+            const board = await boardService.getById(params.boardId)
+            setCurrboard(board)
+            let boardStyleColor = board.style.backgroundColor
+            if (!boardStyleColor) return
+
+            boardStyleColor = darken(0.1, boardStyleColor)
+            const transparentColor = transparentize(0.8, boardStyleColor)
+
+            setNavColor(transparentColor)
+         } catch (err) {
+            console.log('failed to change nav color', err)
+         }
       }
+   }
 
-
-      function onCloseModal() {
-          setModalType(null)
+   async function onStarredClick() {
+      const boardCopy = { ...currBoard }
+      boardCopy.isStarred = true
+      try {
+         await updateBoard(boardCopy)
+      } catch (err) {
+         console.log('failed to starred board', err)
       }
+   }
+   console.log('board from header', board)
+   return (
+      <header className='board-header' style={{ backgroundColor: navColor }}>
+         <div className='left'>
+            <h1 className='board-title'>{board?.title}</h1>
 
-     useEffect( () => {
-          fetchBoardStyle()
-          console.log(params)
-     }, [])
+            {board.isStarred ? (
+               <IconButton
+                  Icon={AiFillStar}
+                  text=''
+                  onClick={() => console.log('Clicked!')}
+               />
+            ) : (
+               <IconButton
+                  Icon={AiOutlineStar}
+                  text=''
+                  onClick={() => onStarredClick()}
+               />
+            )}
+         </div>
 
-     async function fetchBoardStyle() {
-          if (params.boardId) {
-               try {
-                    const board = await boardService.getById(params.boardId)
-                    setCurrboard(board)
-                    let boardStyleColor = board.style.backgroundColor
-                    if (!boardStyleColor) return
+         <div className='right'>
+            <IconButton
+               Icon={BsFilter}
+               text='Filter'
+               onClick={(ev) => onToggleModal('filter', ev)}
+            />
+            <div className='board-header-members-container'>
+               {board.members.map((member) => {
+                  return <img src={member.imgUrl} />
+               })}
+            </div>
+            <IconButton
+               Icon={IoPersonAddOutline}
+               text='Share'
+               className='share-board-btn '
+               onClick={(ev) => onToggleModal('addMember', ev)}
+            />
 
-                    boardStyleColor = darken(0.1, boardStyleColor)
-                    const transparentColor = transparentize(
-                         0.8,
-                         boardStyleColor
-                    )
+            <IconButton
+               Icon={IoEllipsisHorizontalSharp}
+               text=''
+               onClick={(ev) => onToggleModal('activity', ev)}
+            />
+         </div>
 
-                    setNavColor(transparentColor)
-               } catch (err) {
-                    console.log('failed to change nav color', err)
-               }
-          } 
-     }
-
-     async function onStarredClick() {
-          const boardCopy = { ...currBoard }
-          boardCopy.isStarred = true
-          try {
-               await updateBoard(boardCopy)
-          } catch (err) {
-               console.log('failed to starred board', err)
-          }
-     }
-
-     return (
-          <header
-               className='board-header'
-               style={{ backgroundColor: navColor }}
-          >
-               <div className='left'>
-                    <h1 className='board-title'>{board?.title}</h1>
-
-                    {board.isStarred ? (
-                         <IconButton
-                              Icon={AiFillStar}
-                              text=''
-                              onClick={() => console.log('Clicked!')}
-                         />
-                    ) : (
-                         <IconButton
-                              Icon={AiOutlineStar}
-                              text=''
-                              onClick={() => onStarredClick()}
-                         />
-                    )}
-               </div>
-
-               <div className='right'>
-                    <IconButton
-                         Icon={BsFilter}
-                         text='Filter'
-                         onClick={(ev) => onToggleModal('filter', ev)}
-                    />
-                    <IconButton
-                         Icon={IoPersonAddOutline}
-                         text='Share'
-                         className='share-board-btn '
-                         onClick={(ev) => onToggleModal('addMember', ev)}
-                    />
-                    <IconButton
-                         Icon={IoEllipsisHorizontalSharp}
-                         text=''
-                         onClick={(ev) => onToggleModal('activity', ev)}
-                    />
-               </div>
-
-               {modalType && <DynamicActionModal
-                        cmpType={modalType}
-                        modalTitle={modalTitles[modalType]}
-                        event={eventRef.current}
-                        boardId={params.boardId}
-                        onCloseModal={onCloseModal}
-                    />}
-          </header>
-     )
+         {modalType && (
+            <DynamicActionModal
+               cmpType={modalType}
+               modalTitle={modalTitles[modalType]}
+               event={eventRef.current}
+               boardId={params.boardId}
+               onCloseModal={onCloseModal}
+            />
+         )}
+      </header>
+   )
 }
 
 const IconButton = ({
-     Icon,
-     text = '',
-     style = {},
-     className = 'svg-btn',
-     ...props
+   Icon,
+   text = '',
+   style = {},
+   className = 'svg-btn',
+   ...props
 }) => {
-     const defaultStyle = {}
-     IconButton.propTypes = {
-          Icon: PropTypes.elementType.isRequired,
-          text: PropTypes.string,
-          style: PropTypes.object,
-     }
-     return (
-          <button style={defaultStyle} className={className} {...props}>
-               <Icon style={{ marginRight: text ? '8px' : '0' }} />
-               {text}
-          </button>
-     )
+   const defaultStyle = {}
+   IconButton.propTypes = {
+      Icon: PropTypes.elementType.isRequired,
+      text: PropTypes.string,
+      style: PropTypes.object,
+   }
+   return (
+      <button style={defaultStyle} className={className} {...props}>
+         <Icon style={{ marginRight: text ? '8px' : '0' }} />
+         {text}
+      </button>
+   )
 }
