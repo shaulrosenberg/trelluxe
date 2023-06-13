ce2b4ae77148d5387b2fd781c137469b3e5f8ff4
Dor cohen <78268224+plutus777@users.noreply.github.com>
2023-06-13 16:17:17 +0300

    activity-content images feature added

--- frontend/src/cmps/activity-background.jsx
+++ frontend/src/cmps/activity-background.jsx
@@ -5,19 +5,34 @@ import { ActivityColors } from './activity-colors'
 
 export function ActivityBackgroundColor() {
    const [isColorsOpen, setIsColorsOpen] = useState(false)
+   const [isImagesOpen, setIsImageOpen] = useState(false)
 
    return isColorsOpen ? (
       <ActivityColors />
    ) : (
       <section className='activity-background-container curser-pointer'>
-         <div
-            onClick={() => setIsColorsOpen(true)}
-            className='activity-background-section cursor-pointer'
-         >
-            <div></div>
-            <div></div>
+         <div className='activity-style-container'>
+            <div
+               onClick={() => setIsColorsOpen(true)}
+               className='activity-background-section cursor-pointer'
+            >
+               <div></div>
+               <div></div>
+            </div>
+            {/* <h2 className='activity-text-style'>Colors</h2> */}
+
+            <div
+               onClick={() => setIsImageOpen(true)}
+               className='activity-background-images cursor-pointer'
+            >
+               <img src='https://cdn.pixabay.com/photo/2023/05/27/12/20/eastern-spinebill-8021395_1280.jpg' />
+            </div>
+            {/* <h2 className='activity-text-style'>Images</h2> */}
          </div>
-         <h2 className='activity-text-style'>Colors</h2>
+         <div className='activity-style-info'>
+            <h2 className='activity-text-style'>Colors</h2>
+            <h2 className='activity-text-style'>Images</h2>
+         </div>
       </section>
    )
 }
