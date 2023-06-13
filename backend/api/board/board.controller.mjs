bcb24437961e9772416c50f2da4cddf151e11ada
shaulrosenberg <shaulr10@gmail.com>
2023-06-13 22:05:44 +0300

    ai assistant

--- backend/api/board/board.controller.mjs
+++ backend/api/board/board.controller.mjs
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import https from 'https'
 import { config } from 'dotenv'
 import { boardService } from './board.service.mjs'
 import { logger } from '../../services/logger.service.mjs'
@@ -7,28 +8,71 @@ import { asyncLocalStorage } from '../../services/als.service.mjs'
 config()
 
 // open ai
-export async function processCommand(req, res) {
-	const commandText = req.body.commandText
-	console.log(process.env.OPENAI_API_KEY)
-	try {
-		const response = await axios.post('https://api.openai.com/v4/engines/davinci-codex/completions',
-			{
-				prompt: `Translate the following English command to a task: ${commandText}`,
-				max_tokens: 60,
-				temperature: 0.5,
-			},
-			{
-				headers: {
-					'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`,
-					'Content-Type': 'application/json',
-				},
-			}
-		);
+// export async function processCommand(req, res) {
+// 	const commandText = req.body.commandText
+// 	console.log(process.env.OPENAI_API_KEY)
+// 	try {
+// 		const response = await axios.post('https://api.openai.com/v4/engines/davinci-codex/completions',
+// 			{
+// 				prompt: `Translate the following English command to a task: ${commandText}`,
+// 				max_tokens: 60,
+// 				temperature: 0.5,
+// 			},
+// 			{
+// 				headers: {
+// 					'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`,
+// 					'Content-Type': 'application/json',
+// 				},
+// 			}
+// 		);
 
-		res.json(response.data);
-	} catch (error) {
-		res.status(500).json({ error: 'Error processing command' });
-	}
+// 		res.json(response.data);
+// 	} catch (error) {
+// 		res.status(500).json({ error: 'Error processing command' });
+// 	}
+// }
+
+export async function processCommand(req, res) {
+    const commandText = req.body.commandText
+    console.log(process.env.OPENAI_API_KEY)
+    const postData = JSON.stringify({
+        prompt: `Translate the following English command to a task: ${commandText}`,
+        max_tokens: 60,
+        temperature: 0.5,
+    });
+
+    const options = {
+        hostname: 'api.openai.com',
+        port: 443,
+        path: '/v4/engines/davinci-codex/completions',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`,
+            'Content-Length': Buffer.byteLength(postData)
+        }
+    };
+
+    const request = https.request(options, (response) => {
+        let data = '';
+        
+        response.on('data', (chunk) => {
+            data += chunk
+        })
+
+        response.on('end', () => {
+            res.json(JSON.parse(data))
+        })
+    })
+
+    request.on('error', (error) => {
+        console.error(`problem with request: ${error.message}`)
+        res.status(500).json({ error: 'Error processing command' })
+    })
+
+    // write data to request body
+    request.write(postData)
+    request.end()
 }
 
 export async function getBoards(req, res) {
