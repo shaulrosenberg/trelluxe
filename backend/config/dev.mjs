62d48ef41d52791359c6ba0595f768035c97805a
shaulrosenberg <shaulr10@gmail.com>
2023-06-14 19:26:08 +0300

    updated group controls

--- backend/config/dev.mjs
+++ backend/config/dev.mjs
@@ -1,5 +1,6 @@
 export default  {
-  dbURL: process.env.DB_URL,
+  // dbURL: process.env.DB_URL,
+  dbURL: 'mongodb+srv://shaul:shaul123@cluster0.qhknq9y.mongodb.net/?retryWrites=true&w=majority',
   dbName : 'board_db'
 }
 
